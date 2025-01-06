39c0a77661ab421e238364d35eaa3cc874ed1db1
Jekabs Krigerts <jekabs.krigerts@gmail.com>
2025-01-06 11:28:53 +0200

    CHECKPOINT 06.01.25

--- index.php
+++ index.php
@@ -5,14 +5,24 @@ require "Database.php";
 
 $config = require("config.php");
 
-// 1. Izveidot datu bāzi ✅
-// 2. Izveidot savienojumu ar PHP ✅
-// 3. Izvadīt datus uz HTML ✅
+// Mērķis: Uztaisīt filtru - ierakstu meklēšanu
+//  1. Izveidot meklēšanas joslu: HTML forma, kurā ir input un submit poga ✅
+//  2. Kaut kā sarakstīt PHP
+//  3. Atgriezt meklētos datus no SQL datu bāzes
 
 
 $db = new Database($config["database"]);
 $posts = $db->query("SELECT * FROM posts")->fetchAll();
 
+if (isset($_GET["search_query"]) && $_GET["search_query"] != "" ) {
+    $posts = $db->query("SELECT * FROM posts WHERE content LIKE '%" . $_GET["search_query"] . "%';")->fetchAll();
+}
+echo "<h1>Blogs</h1>";
+
+echo "<form>";
+echo "<input name='search_query' />";
+echo "<button>Meklēt</button>";
+echo "</form>";
 
 echo "<ul>";
 foreach($posts as $post) {
